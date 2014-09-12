e064ed2fbe9b571f97800d309617afde17b47229
Clay Miller <clay@smockle.com>
2014-09-11 23:12:57 -0500

    Finish accumulator.

--- accumulator/accumulator.cpp
+++ accumulator/accumulator.cpp
@@ -3,23 +3,24 @@
 // I consulted the following resources for help:
 // http://stackoverflow.com/questions/4654636/how-to-determine-if-a-string-is-a-number-with-c
 // http://www.cplusplus.com/forum/general/13135/
+// http://stackoverflow.com/questions/10847237/how-to-convert-from-int-to-char
 
-Accumulator::Accumulator() {
+Accumulator::Accumulator(Memory memory) {
   a = 0;
 };
 
-int Accumulator::loadi(int value) {
-  a = value;
-  return SUCCESS;
-}
-
 int Accumulator::load(int address) {
-  std::string value = memory.read(address);
+  if (address < 0) {
+    std::cout << COLOR_ERROR << " Memory address cannot be a negative integer." << std::endl;
+    return FAIL;
+  }
+
+  std::string value = spam::memory.read(address);
 
   if (std::regex_match(value, std::regex("[0-9]+"))) {
     a = atoi(value.c_str());
   } else {
-    std::cout << COLOR_ERROR << " Value at memory address cannot be converted to an int." << std::endl;
+    std::cout << COLOR_ERROR << " Value at memory address cannot be converted to an integer." << std::endl;
     return FAIL;
   }
 
@@ -27,14 +28,50 @@ int Accumulator::load(int address) {
 }
 
 int Accumulator::store(int address) {
+  if (address < 0) {
+    std::cout << COLOR_ERROR << " Memory address cannot be a negative integer." << std::endl;
+    return FAIL;
+  }
+
+  std::string s = std::to_string(a);
+  spam::memory.store(address, (char *) s.c_str());
+
   return SUCCESS;
 }
 
 int Accumulator::add(int address) {
+  if (address < 0) {
+    std::cout << COLOR_ERROR << " Memory address cannot be a negative integer." << std::endl;
+    return FAIL;
+  }
+
+  std::string value = spam::memory.read(address);
+
+  if (std::regex_match(value, std::regex("[0-9]+"))) {
+    a = a + atoi(value.c_str());
+  } else {
+    std::cout << COLOR_ERROR << " Value at memory address cannot be converted to an integer." << std::endl;
+    return FAIL;
+  }
+
   return SUCCESS;
 }
 
 int Accumulator::multiply(int address) {
+  if (address < 0) {
+    std::cout << COLOR_ERROR << " Memory address cannot be a negative integer." << std::endl;
+    return FAIL;
+  }
+
+  std::string value = spam::memory.read(address);
+
+  if (std::regex_match(value, std::regex("[0-9]+"))) {
+    a = a * atoi(value.c_str());
+  } else {
+    std::cout << COLOR_ERROR << " Value at memory address cannot be converted to an integer." << std::endl;
+    return FAIL;
+  }
+
   return SUCCESS;
 }
 
