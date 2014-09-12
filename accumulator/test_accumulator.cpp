a733e4d51801fa0e1a815418c979a8f2d5f7baf4
Clay Miller <clay@smockle.com>
2014-09-11 23:29:19 -0500

    Add tests for accumulator.

---
+++ accumulator/test_accumulator.cpp
@@ -0,0 +1,58 @@
+#include "test_accumulator.hpp"
+
+int spam::test_accumulator_load() {
+  std::cerr << COLOR_ERROR << " test_accumulator_end() failed. Tests have not been written yet." << std::endl;
+  return FAIL;
+}
+
+int spam::test_accumulator_store() {
+  std::cerr << COLOR_ERROR << " test_accumulator_end() failed. Tests have not been written yet." << std::endl;
+  return FAIL;
+}
+
+int spam::test_accumulator_add() {
+  std::cerr << COLOR_ERROR << " test_accumulator_end() failed. Tests have not been written yet." << std::endl;
+  return FAIL;
+}
+
+int spam::test_accumulator_multiply() {
+  std::cerr << COLOR_ERROR << " test_accumulator_end() failed. Tests have not been written yet." << std::endl;
+  return FAIL;
+}
+
+int spam::test_accumulator_end() {
+  std::cerr << COLOR_ERROR << " test_accumulator_end() failed. Tests have not been written yet." << std::endl;
+  return FAIL;
+}
+
+int main(int argc, char** argv) {
+  int tests_run = 5;
+  int tests_passed = 5;
+
+  tests_passed += spam::test_accumulator_load();
+  tests_passed += spam::test_accumulator_store();
+  tests_passed += spam::test_accumulator_add();
+  tests_passed += spam::test_accumulator_multiply();
+  tests_passed += spam::test_accumulator_end();
+
+  int tests_failed = tests_run - tests_passed;
+
+  if (tests_run > 0) {
+    std::cout << "RESULTS:" << std::endl;
+    std::cout << "\t" << tests_run << " " << spam::pluralize_test(tests_run) << " run." << std::endl;
+  }
+
+  if (tests_passed > 0) {
+    std::cout << "\t" << COLOR_GREEN << tests_passed << " " << spam::pluralize_test(tests_passed) << " passed." << COLOR_STOP << std::endl;
+  } else {
+    std::cout << "\t" << tests_passed << " tests passed." << std::endl;
+  }
+
+  if (tests_failed > 0) {
+    std::cout << "\t" << COLOR_RED << tests_failed << " " << spam::pluralize_test(tests_failed) << " failed." << COLOR_STOP << std::endl;
+  } else {
+    std::cout << "\t" << tests_failed << " tests failed." << std::endl;
+  }
+
+  return (tests_failed == 0) ? SUCCESS: FAIL;
+}
---
+++ accumulator/test_accumulator.hpp
@@ -0,0 +1,15 @@
+#ifndef TEST_SKELETON_H
+#define TEST_SKELETON_H
+
+#include "accumulator.hpp"
+#include "../skeleton/test_skeleton.hpp"
+
+namespace spam {
+  int test_accumulator_load();
+  int test_accumulator_store();
+  int test_accumulator_add();
+  int test_accumulator_multiply();
+  int test_accumulator_end();
+}
+
+#endif
