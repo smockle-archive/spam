c1961a40b62d564a2402335dbe9fbcd74b79e6d1
Clay Miller <clay@smockle.com>
2014-10-01 16:12:41 -0500

    Fix clang errors.

--- gpr/gpr.hpp
+++ gpr/gpr.hpp
@@ -1,29 +1,29 @@
 #ifndef GPR_H
 #define GPR_H
 
+#import "../registry/registry.hpp"
+#import "../memory/memory.hpp"
+
 namespace spam {
+  class GPR {
+      private:
+      public:
+          Registry r;
+          Memory m;
 
-class GPR {
-    private:
-    public:
-        Registry r;
-        Memory m;
+          int addi(int rdest, int rsrc, int imm);
+          int b(char* label);
+          int beqz(int rsrc1, int rsrc2, char* label);
+          int bge(int rsrc1, int rsrc2, char* label);
+          int bne(int rsrc1, int rsrc2, char* label);
+          int la(int rdest, int addr);
+          int lb(int rdest, int addr);
+          int li(int rdest, int imm);
+          int subi(int rdest, int rsrc, int imm);
+          int syscall();
 
-        int addi(int rdest, int rsrc, int imm);
-        int b(char* label);
-        int beqz(int rsrc1, int rsrc2, char* label);
-        int bge(int rsrc1, int rsrc2, char* label);
-        int bne(int rsrc1, int rsrc2, char* label);
-        int la(int rdest, int addr);
-        int lb(int rdest, int addr);
-        int li(int rdest, int imm);
-        int subi(int rdest, rsrc, int imm);
-        int syscall();
-
-        int decodeLabel(char* label);
-}
-
-
+          int decodeLabel(char* label);
+  };
 }
 
 #endif
