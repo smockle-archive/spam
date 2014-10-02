dcb27ff2e9d5d38c6ba8c03a5687d2c78ac61674
Jake H <milieu@javakat.io>
2014-10-01 22:48:15 -0500

    Move towards proper memory initialization

--- skeleton/skeleton.cpp
+++ skeleton/skeleton.cpp
@@ -36,10 +36,72 @@ int spam::Skeleton::do_memory(std::string filename) {
 
     if (std::regex_match(line, std::regex("[a-zA-Z0-9][:]+"))) {
       int stop = line.find(' ');
+      int label_address = memory->lineCount;
+      int current_location = input.tellg();
       std::string label;
+
       label = line.substr(0, (stop >= 0) ? stop : std::string::npos);
-      //loop through the entire program, replacing all instances of `label`
-      //with the current lineCount
+
+
+
+      // Loop through the entire program, replacing all instances of `label`
+      // with the current lineCount
+      // Note: we must also check already-processed memory.
+
+      // Checking memory!
+      for(int x = D_BASE_ADDR; x < mem_d_addr; x++) {
+        std::string ins(memory->read(x));
+        // If it has a colon, it's a label.
+        int index = ins.find(label);
+        int length = label.length();
+        // If we couldn't find the given label, let's walk to the sky.
+        // Vary the lines our sparklers make spelling our names in the air.
+        // Remember what it's like to be a kid.
+        if (index < 0) continue;
+        // Convert the label_address to a string...
+        char* addr_charp;
+        std::sprintf(addr_charp, "%d", label_address);
+        std::string addr_str(addr_charp);
+        // ...and then replace all instances of the label with the address.
+        ins.replace(index, length, addr_str);
+        memory->store(x, (char*)ins.c_str());
+      }
+      for(int x = T_BASE_ADDR; x < mem_t_addr; x++) {
+        std::string ins(memory->read(x));
+        // If it has a colon, it's a label.
+        int index = ins.find(label);
+        int length = label.length();
+        // If we couldn't find the given label, let's ditch school.
+        // Light cigs behind the bulletin board by that statue of the mascot.
+        // Remember what it's like to be a punk.
+        if (index < 0) continue;
+        // Convert the label_address to a string...
+        char* addr_charp;
+        std::sprintf(addr_charp, "%d", label_address);
+        std::string addr_str(addr_charp);
+        // ...and then replace all instances of the label with the address.
+        ins.replace(index, length, addr_str);
+        memory->store(x, (char*)ins.c_str());
+      }
+      // // Checking ahead in the program!
+      // for(std::string replacer; getline(input, replacer);) {
+      //   // If it has a colon, it's a label.
+      //   int index = replacer.find(label);
+      //   int length = label.length();
+      //   // If we couldn't find the given label, let's have a sleepover.
+      //   // Fight fires with dreams and fly cars in video games.
+      //   // Remember what it's like to have fun.
+      //   if (index < 0) continue;
+      //   // Convert the label_address to a string...
+      //   char* addr_charp;
+      //   std::sprintf(addr_charp, "%d", label_address);
+      //   std::string addr_str(addr_charp);
+      //   // ...and then replace all instances of the label with the address.
+      //   replacer.replace(index, length, addr_str);
+      //   memory->store(x, (char*)replacer.c_str());
+      // }
+      // Set the input stream back to where it was when we hit this label.
+      input.seekg(current_location, input.beg);
     }
     else if (line.compare(".data") == 0) {
       buffer = 'd';
