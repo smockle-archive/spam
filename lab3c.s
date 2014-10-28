a818ead9bd2bf5355343ebae5c2f14b494cbd349
Clay Miller <clay@smockle.com>
2014-10-27 23:41:11 -0500

    Add an end.

--- lab3c.s
+++ lab3c.s
@@ -1,14 +1,14 @@
 
 	.text
 
-main:	
+main:
     li 1, 0
 	li 2, 10
 	li 3, 100
 	li 4, 1000
 	li 5, 7
 
-loop:	
+loop:
     subi 5, 5, 1
 	add 1, 1, 2
 	add 1, 1, 3
@@ -22,4 +22,4 @@ loop:
 
 	li 2, 10
 	syscall
-	
+	end
